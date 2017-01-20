6dd031690d1d311ee7e14c5bcd29d0b322568370
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-01-20 09:22:04 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function change(rotate){
+  document.getElementById(rotate).style.backgroundColor="red"
+}
+function changeb(rotate){
+  document.getElementById(rotate).style.backgroundColor="green";
+}
+function change(skew){
+  document.getElementById(rotate).style.backgroundColor="red"
+}
